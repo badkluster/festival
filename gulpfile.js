5083030070800b76c97c6010fb6f37f5fcc46918
badkluster <cabronator_pesuti@hotmail.com>
2021-06-28 17:08:49 -0300

    Hola mundo

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,70 @@
-const { series, src, dest, watch } = require("gulp");
+const { series, src, dest, watch, parallel } = require("gulp");
 var sass = require("gulp-sass")(require("sass"));
+const imagemin = require("gulp-imagemin");
+const notify = require("gulp-notify");
+const webp = require("gulp-webp");
+// const concat = require("gulp-concat");
 
-//Funcion que compula SASS
+// Utilidades CSS
+// const autoprefixer = require("autoprefixer");
+// const postcss = require("gulp-postcss");
+// const cssnano = require("cssnano");
+// const sourcemaps = require("gulp-sourcemaps");
+
+// Utilidades JS
+// const terser = require("gulp-terser-js");
+// const rename = require("gulp-rename");
+
+const paths = {
+  imagenes: "src/img/**/*",
+  scss: "src/scss/**/*.scss",
+  js: "src/js/**/*.js",
+};
 
 function css() {
-  return src("src/scss/app.scss")
-    .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
-    .pipe(dest("./build/css"));
+  return (
+    src(paths.scss)
+      // .pipe(sourcemaps.init())
+      .pipe(sass().on("error", sass.logError))
+      // .pipe(postcss([autoprefixer(), cssnano()]))
+      // .pipe(sourcemaps.write("."))
+      .pipe(dest("./build/css"))
+  );
 }
 
-function minificarcss() {
-  return src("src/scss/app.scss")
-    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
-    .pipe(dest("./build/css"));
+// function javascript() {
+//   return src(paths.js)
+//     .pipe(sourcemaps.init())
+//     .pipe(concat("bundle.js"))
+//     .pipe(terser())
+//     .pipe(sourcemaps.write("."))
+//     .pipe(rename({ suffix: ".min" }))
+//     .pipe(dest("./build/js"));
+// }
+
+function imagenes() {
+  return src(paths.imagenes)
+    .pipe(imagemin())
+    .pipe(dest("./build/img"))
+    .pipe(notify({ message: "Imagen Minificada" }));
 }
 
-function watcharchivos() {
-  watch("src/scss/**/*.scss", css); // un * = a carpeta  actul **/* = todos los archivos con esa extension en todas las carpetas
+function versionWebp() {
+  return src(paths.imagenes)
+    .pipe(webp())
+    .pipe(dest("./build/img"))
+    .pipe(notify({ message: "Versión webP lista" }));
+}
+
+function watchArchivos() {
+  watch(paths.scss, css); // * = La carpeta actual - ** = Todos los archivos con esa extensión
+  // watch(paths.js, javascript);
 }
 
 exports.css = css;
-exports.minificarcss = minificarcss;
-exports.watcharchivos = watcharchivos;
+exports.imagenes = imagenes;
+exports.watchArchivos = watchArchivos;
+
+exports.default = series(css, imagenes, versionWebp, watchArchivos);
+
+//descargar npm i --save-dev gulp-notify / gulp-imagemin / gulp-sass / gulp-webp
